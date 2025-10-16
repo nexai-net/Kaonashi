54ad8ad5a546e4727b7a71ba1deb69aa0b8c6d02
Jerome Fortias <jfortias@nexaisoftware.net>
2025-10-16 08:39:17 +0200

    Addd Symbolic Encoder class

---
+++ KaonashiClient/Models/SymbolicEncoder.cs
@@ -0,0 +1,16 @@
+namespace Localhost.AI.Kaonashi
+{
+    public class SymbolicEncoder : EntityBase
+    {
+        public string Name { get; set; } = string.Empty;
+        public List<string> Regex { get; set; } = new List<string>();
+        public List<string> Terms { get; set; } = new List<string>();
+        public List<string> Should { get; set; } = new List<string>();
+        public List<string> ShouldNot { get; set; } = new List<string>();
+        public List<string> Must { get; set; } = new List<string>();
+        public List<string> MustNot { get; set; } = new List<string>();
+        public string SystemPrompt { get; set; } = string.Empty;
+
+    }
+
+}
